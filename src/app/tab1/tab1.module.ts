3599c01568b5217cd6aee7552543c1d4148d8f71
Slawomir Zamora <slawomir.zamora@ziflow.com>
2022-09-13 21:44:34 +0200

    added missing files

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { ProductFormModule } from './product-form/product-form.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
@@ -13,7 +12,6 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     Tab1PageRoutingModule,
     ProductFormModule,
   ],
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  msg = 'tekst... ';
 
   constructor() {}
 
+  @HostListener('window:keydown', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log('event: ', event);
+    console.log('event.key:', event.key);
+    this.msg += event.key;
+  }
 }
