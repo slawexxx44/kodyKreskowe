41ce25a81ca24b7d1eb317e0f638dc26d2a7dd74
Slawomir Zamora <slawomir.zamora@ziflow.com>
2022-09-13 21:02:25 +0200

    version1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.kodykreskowe',
   appName: 'Kody Kreskowe',
   webDir: 'www',
   bundledWebRuntime: false
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { ProductFormModule } from './product-form/product-form.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
@@ -13,8 +14,9 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    ProductFormModule,
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page],
 })
 export class Tab1PageModule {}
