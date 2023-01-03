8d682df1f7ab907e5fd55217094d08094fc9d76b
Slawomir Zamora <slawomir.zamora@ziflow.com>
2023-01-03 09:53:40 +0100

    feat (assign-context) changes for clickable area

---
+++ src/app/components/assign-contexts/contexts.page.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CommunicationService } from '../../services/communication.service';
+import { IDostawy, HttpService } from '../../services/http.service';
+import { FormService } from '../../services/form-service.service';
+import { TabService } from '../current-tab.service';
+import { NavController } from '@ionic/angular';
+import { appRoutes } from '../../shared/routes/routes';
+
+@Component({
+  selector: 'app-contexts-location',
+  templateUrl: 'contexts.page.html',
+})
+export class ContextsPage implements OnInit {
+  contexts: { [key: string]: IDostawy[] };
+
+  constructor(
+    private communicationService: CommunicationService,
+    private http: HttpService,
+    private formService: FormService,
+    public tabService: TabService,
+    private router: NavController
+  ) {}
+
+  ngOnInit(): void {
+    this.http.getDostawy().subscribe((d) => {
+      console.log(d);
+      const obj = {};
+      d.dostawy.forEach((dostawa) => {
+        if (dostawa.koN.length > 0) {
+          obj[dostawa.koN] = obj[dostawa.koN] || [];
+          obj[dostawa.koN].push(dostawa);
+        } else {
+          obj[dostawa.nr_zam] = [];
+        }
+      });
+
+      console.log(obj);
+      this.contexts = obj;
+    });
+  }
+
+  navigate() {
+    this.router.navigateRoot(appRoutes.productToGroups);
+  }
+
+  async selectContext(nrZam: string) {
+    console.log('kontekts', nrZam);
+    const response = await this.formService.openContextForm();
+    if (!response) {
+      return;
+    }
+    this.http.setContext(nrZam, response.code).subscribe(
+      () => {
+        this.communicationService.presentToast('Kontekt ustawiony pomyślnie');
+        this.navigate();
+      },
+      () => {
+        this.communicationService.presentToast(
+          'Błąd ustawiania kontekstu',
+          'danger'
+        );
+      }
+    );
+  }
+}
